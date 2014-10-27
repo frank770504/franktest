bdd526a40477d024de987cd00f077e43765e089e
frank.yw.chen <frank.yw.chen@acer.com>
2014-10-27 20:10:14 +0800

    add code to read acc info

--- gen/com/example/batchmodetest/R.java
+++ gen/com/example/batchmodetest/R.java
@@ -1636,6 +1636,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020033;
     }
     public static final class id {
+        public static final int TextView_accInfo=0x7f050041;
         public static final int action_bar=0x7f050033;
         public static final int action_bar_activity_content=0x7f05001a;
         public static final int action_bar_container=0x7f050032;
@@ -1649,7 +1650,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f050030;
         public static final int action_mode_bar_stub=0x7f05002f;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050023;
         public static final int always=0x7f05000d;
         public static final int beginning=0x7f050012;
