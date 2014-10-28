cdf426f01850bb338cea82b528a5df3e7f683312
frank.yw.chen <frank.yw.chen@acer.com>
2014-10-28 14:04:08 +0800

    rrrrrrearrange

--- src/com/example/batchmodetest/MainActivity.java
+++ src/com/example/batchmodetest/MainActivity.java
@@ -32,67 +32,69 @@ public class MainActivity extends ActionBarActivity {
 
 	SensorManager sensorManager;
 
-    TextView textInfo;
-    String strSensor;	
-	
-    @SuppressLint("NewApi")
+	TextView textInfo;
+	String strSensor;
+
+	@SuppressLint("NewApi")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-		//--Initializing accelerometer--//
-	    sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// --Initializing accelerometer--//
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		boolean accelerometerPresent;
 		Sensor accelerometerSensor;
-	    List<Sensor> sensor_TYPE_ACCELEROMETER_List = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
-	    //////////////////////////////////
-	    
-	    if(sensor_TYPE_ACCELEROMETER_List.size() > 0){
-          accelerometerPresent = true;
-          accelerometerSensor = sensor_TYPE_ACCELEROMETER_List.get(0);
-          
-          strSensor  = "Name: " + accelerometerSensor.getName()
-        		  + "\nVersion: " + String.valueOf(accelerometerSensor.getVersion())
-        		  + "\nVendor: " + accelerometerSensor.getVendor()
-        		  + "\nType: " + String.valueOf(accelerometerSensor.getType())
-        		  + "\nMax: " + String.valueOf(accelerometerSensor.getMaximumRange())
-        		  + "\nResolution: " + String.valueOf(accelerometerSensor.getResolution())
-        		  + "\nPower: " + String.valueOf(accelerometerSensor.getPower())
-        		  + "\nClass: " + accelerometerSensor.getClass().toString();
+		List<Sensor> sensor_TYPE_ACCELEROMETER_List = sensorManager
+				.getSensorList(Sensor.TYPE_ACCELEROMETER);
+		// ////////////////////////////////
 
-	    }
-	    else{
-	    	accelerometerPresent = false;
-	    }	 
-	    
-	    setContentView(R.layout.activity_main);
-	    textInfo = (TextView)findViewById(R.id.TextView_accInfo);
-	    
-	    if(strSensor.isEmpty())
-	    	textInfo.setText("Info: ");
-	    else
-	    	textInfo.setText(strSensor);
-	    
-        
-    }
+		if (sensor_TYPE_ACCELEROMETER_List.size() > 0) {
+			accelerometerPresent = true;
+			accelerometerSensor = sensor_TYPE_ACCELEROMETER_List.get(0);
 
+			strSensor = "Name: " + accelerometerSensor.getName()
+					+ "\nVersion: "
+					+ String.valueOf(accelerometerSensor.getVersion())
+					+ "\nVendor: " + accelerometerSensor.getVendor()
+					+ "\nType: "
+					+ String.valueOf(accelerometerSensor.getType()) + "\nMax: "
+					+ String.valueOf(accelerometerSensor.getMaximumRange())
+					+ "\nResolution: "
+					+ String.valueOf(accelerometerSensor.getResolution())
+					+ "\nPower: "
+					+ String.valueOf(accelerometerSensor.getPower())
+					+ "\nClass: " + accelerometerSensor.getClass().toString();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		} else {
+			accelerometerPresent = false;
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		setContentView(R.layout.activity_main);
+		textInfo = (TextView) findViewById(R.id.TextView_accInfo);
+
+		if (strSensor.isEmpty())
+			textInfo.setText("Info: ");
+		else
+			textInfo.setText(strSensor);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
